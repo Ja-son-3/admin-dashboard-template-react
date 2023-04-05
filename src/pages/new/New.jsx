fd69a739c4ddb98e035dda04af9bd59df12a96bf
Jason Huang <jason97521@gmail.com>
2023-04-05 07:47:40 -0400

    new users and products pages

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,11 +1,69 @@
 import "./new.scss"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import { DriveFolderUploadOutlined } from "@mui/icons-material"
+import { useState } from "react"
 
-const New = () => {
+const New = ({inputs, title}) => {
+
+  const [file,setFile] = useState("")
   return (
     <div className="new">
-        new
+        <Sidebar />
+        <div className="newContainer">
+          <Navbar />
+          <div className="top">
+            <h1>{title}</h1>
+          </div>
+          <div className="bottom">
+            <div className="left">
+              <img src={file ? URL.createObjectURL(file) : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"} alt="" />
+            </div>
+            <div className="right">
+              <form>
+                <div className="formInput">
+                  Image: <label htmlFor="file"><DriveFolderUploadOutlined className="icon"/></label>
+                  <input type="file" id="file" onChange={(e)=>setFile(e.target.files[0])} style={{display: "none",}}/>
+                </div>
+
+                {inputs.map(input=>(
+                  <div className="formInput" key={input.id}>
+                    <label>{input.label}</label>
+                    <input type={input.type} placeholder={input.placeholder} />
+                  </div>
+                ))}
+                <button>Send</button>
+              </form>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
 
 export default New
+
+{/* <div className="formInput">
+<label>Name and Surname</label>
+<input type="text" placeholder="John Doe" />
+</div>
+<div className="formInput">
+<label>Email</label>
+<input type="email" placeholder="john_doe@gmail.com" />
+</div>
+<div className="formInput">
+<label>Phone</label>
+<input type="text" placeholder="+1 123 345 6798" />
+</div>
+<div className="formInput">
+<label>Password</label>
+<input type="password" />
+</div>
+<div className="formInput">
+<label>Address</label>
+<input type="text" placeholder="Elton St. 216 New York" />
+</div>
+<div className="formInput">
+<label>Country</label>
+<input type="text" placeholder="USA" />
+</div> */}
